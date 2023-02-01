32093c611e7d68c642ff6d26b5b5d3ddc3273cf7
Emilie Ewing <2653131e@student.gla.ac.uk>
2023-02-01 21:39:11 +0000

    chapter 7 without exercises

---
+++ rango/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from rango.models import Page, Category
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    #The user won't be able to enter a value for the hidden fields
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    #we say the slug field isn't required by the form. Our model is responsible for populating this field when the form is saved
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    #An inline class to provide additional information on the form.
+    class Meta:
+        #Provide an association between the ModelForm and a model
+        #enables django to take care of creating a form in the image of the specified model. Also helps with error flagging and saving
+        #and displaying data in the form.
+        model = Category
+        fields = ('name',)
+
+class PageForm(forms.ModelForm):
+    #Even though we have hidden fields, we still need to include that field in the form. E.g. if in fields we excluded views, 
+    #the form would not contain that field despite it being specified and the form wouldn't return the value 0 for that field, 
+    #potentially yielding an error if the model wasn't set up so that these fields had default = 0. 
+    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    #Note that the max_length parameters we supply to each field are idnetical to those we specifed
+    #in the data models
+
+    class Meta:
+        #Providean association between the ModelForm and a model
+        model = Page
+
+        #What fields do we want to include in our form?
+        #This way we don't need every field in the model present.
+        #Some fields may allow NULL values; we may not want to include them.
+        #Here, we are hiding the foreign key.
+        #we can either exclude the category field from the form,
+        exclude = ('category',)
+        #or specify the fields to include (don't include the category field).
+        #fields = ('title', 'url', 'views')
--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
 
 def index(request):
     #Query the database for a list of ALL categories currently stored.
@@ -63,3 +65,34 @@ def show_category(request, category_name_slug):
 
     #go render the response and return it to the client
     return render(request, 'rango/category.html', context=context_dict)
+
+
+def add_category(request):
+    #create a category form
+    form = CategoryForm()
+
+    #A HTTP POST? Did the user submit data via the form
+    if request.method == 'POST':
+        #we handle the POST request through the same URL
+        form = CategoryForm(request.POST)
+
+        #Have we been provided with a valid form?
+        if form.is_valid():
+            #Save the new category to the database
+            form.save(commit=True)
+            #now that the category is saved, we could confirm this.
+            #For now, just redirect the user back to tne index view
+            return redirect('/rango/')
+        else:
+            #the supplied form contains errors - just print them to the terminal
+            print(form.errors)
+
+    #handles the three scenarios:
+    #- showing a new, blank form for adding a category
+    #- saving form data provided by the user to the associated model, and redirecting the user to the Rango homepage, and
+    #- if there are any errors, redisplay the form with error messages
+
+    #will handle the bad form, new form or no form cases. Render the form with error messages (if any).
+    #the add_category.html page is a template which contains the relevant Django template code and HTML for the form and page.
+    return render(request, 'rango/add_category.html', {'form':form})
+
